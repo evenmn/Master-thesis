74913906be101861c4b09c9cbf22ab9f056d4a50
evenmn <evenmn@fys.uio.no>
2018-09-18 23:34:45 +0200

    Added some functions for faster energy calculations

--- doc/src/Qgas/test.cpp
+++ doc/src/Qgas/test.cpp
@@ -31,15 +31,16 @@ void test_energy_convergence(double energy, double omega, int M, bool interactio
 void test_E_L_calc(){
     int M = 2;
     int N = 2;
+    int D = 2;
+    int norbitals = 1;
     int sampling   = 2;
     double sigma_sqrd = 1;
     double omega = 1.0;
     double omega_sqrd = omega*omega;
-    double D = 2;
     int interaction = 0;
 
     WaveFunction Psi;
-    Psi.setTrialWF(N, M, D, sampling, sigma_sqrd, omega);
+    Psi.setTrialWF(N, M, D, norbitals, sampling, sigma_sqrd, omega);
 
     MatrixXd W       = MatrixXd::Zero(M, N);
     VectorXd a       = VectorXd::Zero(M);
@@ -55,13 +56,13 @@ void test_E_L_calc(){
 
 
     double E = 0;
-    double E_k = 0;
+    double E_kin = 0;
     double E_ext = 0;
     double E_int = 0;
 
-    E = Psi.EL_calc(X, Xa, v, W, D, interaction, E_k, E_ext, E_int);
+    E = Psi.EL_calc(X, Xa, v, W, interaction, E_kin, E_ext, E_int);
     cout << "E: " << E << endl;
-    cout << "E_k: " << E_k << endl;
+    cout << "E_kin: " << E_kin << endl;
     cout << "E_ext: " << E_ext << endl;
     cout << "E_int: " << E_int << endl;
 
